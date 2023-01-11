877cec501161de120d785da44e35e3f628ce9db3
Akshay <akshay@fluidtouch.biz>
2023-01-11 13:30:46 +0530

    Added Package dependencies

--- Package.swift
+++ Package.swift
@@ -1,28 +1,37 @@
 // swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+
 import PackageDescription
+
+let version = "4.7.0"
+let checksum = "7e578f7734ff25796acc2847163c86c857a11351a2f99b220d11189639786ee9"
 
 let package = Package(
     name: "OpenCV",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v11), .macCatalyst(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "OpenCV",
-            targets: ["OpenCV"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["opencv2", "opencv2-dependencies"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(name: "opencv2",
+                      url: "https://github.com/Yeatse/OpenCV-SPM/releases/download/\(version)/opencv2.xcframework.zip",
+                      checksum: checksum),
         .target(
-            name: "OpenCV",
-            dependencies: []),
-        .testTarget(
-            name: "OpenCVTests",
-            dependencies: ["OpenCV"]),
+            name: "opencv2-dependencies",
+            linkerSettings: [
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("CoreImage"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("CoreVideo", .when(platforms: [.iOS])),
+                .linkedFramework("Accelerate", .when(platforms: [.iOS, .macOS])),
+                .linkedFramework("OpenCL", .when(platforms: [.macOS])),
+                .linkedLibrary("c++")
+            ]
+        )
     ]
 )
